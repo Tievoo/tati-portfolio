7153b523c60458fa25f071d2b119f8f77d942632
Tievo <tievolib@gmail.com>
2025-06-07 20:55:06 -0300

    aa

--- src/scripts/Images.ts
+++
@@ -1,9 +0,0 @@
-import pic from '../assets/covers/bya.png'
-
-export function test() {
-    console.log('test function in Images.ts');
-    console.log(pic);
-    console.log(pic.width)
-    console.log(pic.height);
-    console.log(pic.src);
-}
