d28488941277d9e8261a59d97310982ca318644b
Tievo <tievolib@gmail.com>
2025-06-07 20:54:48 -0300

    aaa

---
+++ src/scripts/Images.ts
@@ -0,0 +1,9 @@
+import pic from '../assets/covers/bya.png'
+
+export function test() {
+    console.log('test function in Images.ts');
+    console.log(pic);
+    console.log(pic.width)
+    console.log(pic.height);
+    console.log(pic.src);
+}
